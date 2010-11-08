9e07772c711bd29ba729d6df9e361726ad30c2ee
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-08 15:54:10 +0900

    buildscript submodule

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-copy buildscript\make_new.sh .\
-bash make_new.sh reloadprogressively version=0
-del make_new.sh
+copy buildscript\makexpi.sh .\
+bash makexpi.sh reloadprogressively version=0
+del makexpi.sh
